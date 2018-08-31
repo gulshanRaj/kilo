85c6ea58ed80b77d417027252835d40480ce9283
Gulshan Raj <gulshanraj116@gmail.com>
2018-08-31 23:59:08 +0530

    move cursor with w,a,s,d keys

--- kilo.c
+++ kilo.c
@@ -12,6 +12,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 /*** data ***/
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -145,7 +146,10 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
-  abAppend(&ab, "\x1b[H", 3);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
@@ -153,6 +157,25 @@ void editorRefreshScreen() {
 }
 
 /*** input ***/
+
+void editorMoveCursor(char key) {
+  switch(key) {
+    case 'a':
+      E.cx--;
+      break;
+    case 'd':
+      E.cx++;
+      break;
+    case 'w':
+      E.cy--;
+      break;
+    case 's':
+      E.cy++;
+      break;
+  }
+}
+
+
 void editorProcessKeypress() {
   char c = editorReadKey();
   switch (c) {
@@ -161,6 +184,12 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+    case 'w':
+    case 's':
+    case 'a':
+    case 'd':
+      editorMoveCursor(c);
+      break;
   }
 }
 
@@ -168,6 +197,7 @@ void editorProcessKeypress() {
 /*** init ***/
 
 void initEditor() {
+  E.cx = 0; E.cy = 0;
   if(getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
